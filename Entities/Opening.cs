ada7b5acfb9b5b54ad35f1d45e676d9d10c95e4a
csmark97 <36475822+csmark97@users.noreply.github.com>
2019-05-14 00:16:29 +0200

    Unneccessery comments removed

--- Entities/Opening.cs
+++ Entities/Opening.cs
@@ -13,23 +13,7 @@ namespace CarService.Dal.Entities
         public virtual OpeningDay Thursday { get; set; }
         public virtual OpeningDay Friday { get; set; }
         public virtual OpeningDay Saturday { get; set; }
-        public virtual OpeningDay Sunday { get; set; }
-
-        //public DateTime StartMonday { get; set; }
-        //public DateTime StartTuesday { get; set; }
-        //public DateTime StartWednesday { get; set; }
-        //public DateTime StartThursday { get; set; }
-        //public DateTime StartFriday { get; set; }
-        //public DateTime? StartSaturday { get; set; }
-        //public DateTime? StartSunday { get; set; }
-
-        //public DateTime EndMonday { get; set; }
-        //public DateTime EndTuesday { get; set; }
-        //public DateTime EndWednesday { get; set; }
-        //public DateTime EndThursday { get; set; }
-        //public DateTime EndFriday { get; set; }
-        //public DateTime? EndSaturday { get; set; }
-        //public DateTime? EndSunday { get; set; }
+        public virtual OpeningDay Sunday { get; set; }        
 
         public bool SaturdayOpen { get; set; }
         public bool SundayOpen{ get; set; }
