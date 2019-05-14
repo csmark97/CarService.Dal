891ff0cb2e1484a778813fb252a4363c5080d422
csmark97 <36475822+csmark97@users.noreply.github.com>
2019-05-14 04:36:00 +0200

    bug fixes on fileupload

--- Entities/Car.cs
+++ Entities/Car.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CarService.Dal.Entities
@@ -13,10 +14,20 @@ namespace CarService.Dal.Entities
         public string LicensePlateNumber { get; set; }
         public int YearOfManufacture { get; set; }
         public int Km { get; set; }
-        public DateTime TechnicalValidity { get; set; }
-        public string Picture { get; set; } //maybe byte
+        public DateTime TechnicalValidity { get; set; }        
         public string ClientUserId { get; set; }
         public virtual ClientUser ClientUser { get; set; }
         public virtual ICollection<Service> Services { get; set; }
+
+        //File properties
+        public string Picture { get; set; }
+
+        [Display(Name = "Public Schedule Size (bytes)")]
+        [DisplayFormat(DataFormatString = "{0:N1}")]
+        public long PictureSize { get; set; }
+
+        [Display(Name = "Uploaded (UTC)")]
+        [DisplayFormat(DataFormatString = "{0:F}")]
+        public DateTime PictureUploadDT { get; set; }
     }
 }
---
+++ Migrations/20190514002428_FileUpload.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarService.Dal.Migrations
+{
+    public partial class FileUpload : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "PictureSize",
+                table: "Cars",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PictureUploadDT",
+                table: "Cars",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PictureSize",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "PictureUploadDT",
+                table: "Cars");
+        }
+    }
+}
